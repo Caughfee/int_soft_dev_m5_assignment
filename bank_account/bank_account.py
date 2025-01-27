fa44991d1a49b8da477676384c94048aa1eb74f8
Rogine <menotinfer123@gmail.com>
2025-01-26 22:56:19 -0600

    Completed unit tests for BankAccount class

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -25,99 +25,102 @@ class BankAccount:
             raise ValueError("Enter a valid client number")
 
         if isinstance(balance, float):
-            self.balance = balance
+            self.__balance = balance
         else:
-            self.balance = 0.0
+            self.__balance = 0.0
 
-        @property
-        def account_number(self) -> int:
-            """
-            Accessor for the account number attribute
-            Returns: int - The account number
-            """
-            return self.__account_number
+    @property
+    def account_number(self) -> int:
+        """
+        Accessor for the account number attribute
+        Returns: int - The account number
+        """
+        return self.__account_number
         
-        @property
-        def client_number(self) -> int:
-            """
-            Accessor for the client number attribute
-            Returns: int - the client number
-            """
-            return self.__client_number
+    @property
+    def client_number(self) -> int:
+        """
+        Accessor for the client number attribute
+        Returns: int - the client number
+        """
+        return self.__client_number
         
-        @property
-        def balance(self) -> float:
-            """
-            Accessor for the balance attribute
-            Returns: float - the balance in the bank account
-            """
-            return self.__balance
+    @property
+    def balance(self) -> float:
+        """
+        Accessor for the balance attribute
+        Returns: float - the balance in the bank account
+        """
+        return self.__balance
         
-        def update_balance(self, amount) -> None:
-            """
-            Updates the account balance
-            Args:
-                amount (float): The amount that will be added to the balance
-            """
-            if isinstance(amount, float):
-                self.balance += amount
+    def update_balance(self, amount) -> None:
+        """
+        Updates the account balance
+        Args:
+            amount (float): The amount that will be added to the balance
+        """
+        if isinstance(amount, float):
+            self.__balance += amount
+        else:
+            # If the amount is not a float
+            try:
+                # This will try to check if amount can be converted into a float. Ex) 120 turns into 120.00
+                self.__balance += float(amount)
+            except ValueError:
+                # if all fails, the balance will not be changed
+                print("The amount is not valid")
+
+    def deposit(self, amount: float) -> None:
+        """
+        Allows to make a deposit
+        Args:
+            amount (float): the amount that will be deposited
+        """
+        if isinstance(amount, float):
+            if amount <= 0:
+                raise ValueError(f"Deposit Amount: {amount} must be a positive number")
             else:
-                # If the amount is not a float
-                try:
-                    # This will try to check if amount can be converted into a float. Ex) 120 turns into 120.00
-                    self.balance += float(amount)
-                except ValueError:
-                    # if all fails, the balance will not be changed
-                    print("The amount is not valid")
-
-        def deposit(self, amount: float) -> None:
-            """
-            Allows to make a deposit
-            Args:
-                amount (float): the amount that will be deposited
-            """
-            if isinstance(amount, float):
-                if amount <= 0:
-                    raise ValueError(f"Deposit Amount: {amount} must be a positive number")
+                self.update_balance(amount)
+        else:
+            try:
+                # Just in case the amount is an integer and not a float, this will convert it into a float
+                float_amount = float(amount)
+                if float_amount <= 0:
+                    raise ValueError(f"Deposit Amount: {float_amount} must be a positive number")
                 else:
-                    self.update_balance(amount)
-            else:
-                try:
-                    # Just in case the amount is an integer and not a float, this will convert it into a float
-                    float_amount = float(amount)
-                    if float_amount <= 0:
-                        raise ValueError(f"Deposit Amount: {float_amount} must be a positive number")
-                    else:
-                        self.update_balance(float_amount)
-                except ValueError:
-                    # If the amount is not a numeric
-                    print(f"Deposit amount: {amount} must be numeric")
+                    self.update_balance(float_amount)
+            except ValueError:
+                # If the amount is not a numeric
+                print(f"Deposit amount: {amount} must be numeric")
 
-        def withdrawal(self, amount:float) -> None:
-            """
-            Allows to make withdrawals
-            Args:
-                amount (float): The amount of the withdrawal
-            """
-            if isinstance(amount, float):
-                if amount <= 0:
-                    raise ValueError(f"Withdrawal amount: {amount} must be positive")
-                else:
-                    self.update_balance(-amount)
+    def withdraw(self, amount:float) -> None:
+        """
+        Allows to make withdrawals
+        Args:
+            amount (float): The amount of the withdrawal
+        """
+        if isinstance(amount, float):
+            if amount <= 0:
+                raise ValueError(f"Withdrawal amount: {amount} must be positive")
+            elif amount > self.__balance:
+                raise ValueError(f"Withdrawal amount: {amount} must not exceed the account balance {self.__balance}")
             else:
-                try:
-                    # Just in case the amount is an integer and not a float, this will convert it into a float
-                    float_amount = float(amount)
-                    if float_amount <= 0:
-                        raise ValueError(f"Withdrawal Amount: {float_amount} must be a positive number")
-                    else:
-                        self.update_balance(-float_amount)
-                except ValueError:
-                    # If the amount is not a numeric
-                    print(f"Withdrawal amount: {amount} must be numeric")
+                self.update_balance(-amount)
+        else:
+            try:
+                # Just in case the amount is an integer and not a float, this will convert it into a float
+                float_amount = float(amount)
+                if float_amount <= 0:
+                    raise ValueError(f"Withdrawal Amount: {float_amount} must be a positive number")
+                else:
+                    self.update_balance(-float_amount)
+            except ValueError:
+                # If the amount is not a numeric
+                print(f"Withdrawal amount: {amount} must be numeric")
                 
-
-
-
-        
-
+    def __str__(self) -> str:
+        """
+        Returns a string of that shows the values of the attributes in the class
+        Returns: str - The BankAccount instance as a formatted string
+        """
+        return (f"Account Number: {self.__account_number} Balance: ${self.__balance:.2f}")
--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -5,3 +5,106 @@ Usage: To execute all tests in the terminal execute
 the following command:
     python -m unittest tests/test_bank_account.py
 """
+
+import unittest
+from bank_account.bank_account import BankAccount
+
+class TestBankAccount(unittest.TestCase):
+
+    def setUp(self):
+        # Set up runs automatically before each test method and gives inital values
+        self.bank_account = BankAccount(1, 2, 310.00)
+
+    def test_init_valid(self):
+        # Arrange & Act
+        bank_account = BankAccount(1, 2, 310.00)
+
+        # Assert
+        self.assertEqual(bank_account.account_number, 1)
+        self.assertEqual(bank_account.client_number, 2)
+        self.assertEqual(bank_account.balance, 310.00)
+    
+    def test_balance_invalid(self):
+        # arrange & act
+        bank_account = BankAccount(1, 2, "asdw")
+
+        # assert
+        self.assertEqual(bank_account.balance, 0)
+    
+    def test_invalid_account_number(self):
+        # Arrange & assert
+        with self.assertRaises(ValueError):
+            bank_account = BankAccount("yes", 2, 310.00)
+    
+    def test_invalid_client_number(self):
+        # Arrange & assert
+        with self.assertRaises(ValueError):
+            bank_account = BankAccount(1, "hyes", 310.00)
+    
+    def test_account_number(self):
+        # Set up done above
+
+        # Assert
+        self.assertEqual(self.bank_account.account_number, 1)
+
+    def test_client_number(self):
+        # Set up done above
+
+        # Assert
+        self.assertEqual(self.bank_account.client_number, 2)
+
+    def test_balance(self):
+        # Set up done above
+
+        # Assert
+        self.assertEqual(self.bank_account.balance, 310.00)
+
+    def test_update_balance_positive(self):
+        # Arrange and act
+        self.bank_account.update_balance(100.00)
+
+        # Assert
+        self.assertEqual(self.bank_account.balance, 410.00)
+    
+    def test_update_balance_negative(self):
+        # Arrange and act
+        self.bank_account.update_balance(-100.00)
+
+        # Assert
+        self.assertEqual(self.bank_account.balance, 210.00)
+
+    def test_deposit(self):
+        # Arrange and act
+        self.bank_account.deposit(100.00)
+
+        # Assert
+        self.assertEqual(self.bank_account.balance, 410.00)
+
+    def test_deposit_negative(self):
+        # Arrange Assert
+        with self.assertRaises(ValueError):
+            self.bank_account.deposit(-100.00)
+
+    def test_withdraw(self):
+        # Arrange and act
+        self.bank_account.withdraw(100.00)
+
+        # Assert
+        self.assertEqual(self.bank_account.balance, 210.00)
+
+    def test_withdraw_negative(self):
+        # Arrange and assert
+        with self.assertRaises(ValueError):
+            self.bank_account.withdraw(-100.00)
+
+    def test_withdraw_exceed(self):
+        # Arrange and assert
+        with self.assertRaises(ValueError):
+            self.bank_account.withdraw(400.00)
+
+    def test_str_(self):
+        # Arrange and act
+        expected = ("Account Number: 1 Balance: $310.00")
+
+        # Assert
+        self.assertEqual(expected, str(self.bank_account))
