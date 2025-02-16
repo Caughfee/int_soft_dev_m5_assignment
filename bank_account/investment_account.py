5fa3c163c104b440aea0ebac7afaed54dc1eebc7
Rogine <menotinfer123@gmail.com>
2025-02-16 16:46:37 -0600

    Made sure the management_fee can be converted into a float

--- bank_account/investment_account.py
+++ bank_account/investment_account.py
@@ -1,10 +1,11 @@
 from bank_account.bank_account import BankAccount
-from datetime import date
+from datetime import date, timedelta
 
 class InvestmentAccount(BankAccount):
     """
     A class that represents an investment account
     """
+    TEN_YEARS_AGO = date.today() - timedelta(days=10 * 365.25)
     def __init__(self, account_number: int, client_number: int, balance: float, date_created: date, management_fee: float):
         """
         account_number (int): The account number
@@ -14,3 +15,9 @@ class InvestmentAccount(BankAccount):
             management_fee (float): The flat-rate fee the bank charges for managing an investment account
         """
         super().__init__(account_number, client_number, balance, date_created)
+
+        if isinstance(management_fee, (int, float)):
+            self.__management_fee = float(management_fee)
+        else:
+            self.__management_fee = 2.55
+        
