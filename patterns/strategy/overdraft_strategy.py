de22fec32bbb3fe4be59ee4762112f53a868a59d
Rogine <menotinfer123@gmail.com>
2025-03-10 14:33:13 -0500

    Started the class setup for OverdraftStrategy

---
+++ patterns/strategy/overdraft_strategy.py
@@ -0,0 +1,10 @@
+__author__ = "Rogine Mirando"
+__version__ = "1.0.0"
+
+from patterns.strategy.service_charge_strategy import ServiceChargeStrategy
+
+class OverdraftStrategy(ServiceChargeStrategy):
+    """
+    
+    """
+    
