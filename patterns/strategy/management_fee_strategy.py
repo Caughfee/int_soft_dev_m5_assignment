1e2493518e143184bb72098662f193428b9882a2
Rogine <menotinfer123@gmail.com>
2025-03-13 15:48:35 -0500

    Completed the ManagementFeeStrategy class

--- patterns/strategy/management_fee_strategy.py
+++ patterns/strategy/management_fee_strategy.py
@@ -7,11 +7,29 @@ from bank_account.bank_account import BankAccount
 
 class ManagementFeeStrategy(ServiceChargeStrategy):
     """
-    
+    A subclass of ServiceChargeStrategy that manages service fees based on account creation date
     """
+    TEN_YEARS_AGO = date.today() - timedelta(days = 10 * 365.25)
     def __init__(self, date_created: date, management_fee: float):
         """
-        
+        Initializes the ManagementFeeStrategy based on the values
+        Args:
+            date_created (date): The creation date of the account
+            management_fee (float): The annual management fee for the account
         """
+        self.__date_created = date_created
+        self.__management_fee = management_fee
 
-        
+    def calculate_service_charges(self, account: BankAccount):
+        """
+        Calculates the service charges for the bank account
+        Returns:
+            float: The service charges
+        """
+        if self.__date_created < self.TEN_YEARS_AGO:
+            calculated_service_charge = self.BASE_SERVICE_CHARGE
+            return calculated_service_charge
+        else:
+            calculated_service_charge = self.BASE_SERVICE_CHARGE + self.__management_fee
+            return calculated_service_charge
+
