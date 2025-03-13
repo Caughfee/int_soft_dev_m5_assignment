89c411b2f452651b24463714fef03d749a69894b
Rogine <menotinfer123@gmail.com>
2025-03-13 16:52:57 -0500

    Completed the Subject class

--- patterns/observer/subject.py
+++ patterns/observer/subject.py
@@ -2,10 +2,32 @@ from patterns.observer.observer import Observer
 
 class Subject:
     """
-    
+    A class that represents the Subject in the observer pattern
     """
 
     def __init__(self):
         """
-        
+        Initializes the Subject class
         """
+        self._observers = []
+
+    def attach(self, observer: Observer) -> None:
+        """
+        Attaches an observer
+        """
+        if observer not in self._observers:
+            self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        """
+        Detaches an observer
+        """
+        if observer in self._observers:
+            self._observers.remove(observer)
+
+    def notify(self, message: str) -> None:
+        """
+        Notifies all observers with a message
+        """
+        for observer in self._observers:
+            observer.update(message)
