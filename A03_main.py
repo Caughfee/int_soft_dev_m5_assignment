683d249231718c3e7b6cca8cd1ef2d04284c29bd
Rogine <menotinfer123@gmail.com>
2025-03-16 22:36:43 -0500

    Completed step 2 and 3 in A03

--- A03_main.py
+++ A03_main.py
@@ -18,15 +18,16 @@ from client.client import Client
 
 
 # 2. Create a Client object with data of your choice.
-
+client_1 = Client(101, "Anby", "Demara", "anby.demara@gmail.com")
 
 
 
 # 3a. Create a ChequingAccount object with data of your choice, using the client_number 
 # of the client created in step 2.
+chequing_account = ChequingAccount(123, 101, 1000.00, date(2022, 1, 1), 500.0, 0.05)
 # 3b. Create a SavingsAccount object with data of your choice, using the client_number 
 # of the client created in step 2.
-
+savings_account = SavingsAccount(321, 101, 2000.00, (2023, 1, 1), 100.00)
 
 
 
