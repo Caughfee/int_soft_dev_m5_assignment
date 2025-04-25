00b25496fb2b9bb8933bb18d1b625df48423f29e
Rogine <menotinfer123@gmail.com>
2025-04-24 20:48:43 -0500

    Completed toggle_filter and on_lookup_client

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -86,6 +86,9 @@ class ClientLookupWindow(LookupWindow):
                         item.setTextAlignment(Qt.AlignCenter) # align the rest of the columns to the center
 
         self.account_table.resizeColumnsToContents()
+
+        # toggle filter to indicate data is NOT filtered
+        self.toggle_filter(False)
                 
     @Slot()
     def on_text_changed(self):
@@ -156,4 +159,29 @@ class ClientLookupWindow(LookupWindow):
                 self.account_table.setRowHidden(row, False)
 
                 self.toggle_filter(False)
+
+    def toggle_filter(self, filter_on: bool) -> None:
+        """
+        
+        """
+
+        # Set the filter button true initially
+        self.filter_button.setEnabled(True)
+
+        if filter_on:
+            self.filter_button.setText("Reset")
+            self.filter_combo_box.setEnabled(False)
+            self.filter_edit.setEnabled(False)
+            self.filter_label.setText("Data is Currently Filtered")
+        else:
+            self.filter_button.setText("Apply Filter")
+            self.filter_combo_box.setEnabled(True)
+            self.filter_edit.setEnabled(True)
+            self.filter_edit.setText("")
+            self.filter_combo_box.setCurrentIndex(0) # set the current index to 0
+
+            for row in range(self.account_table.rowCount()):
+                self.account_table.setRowHidden(row, False)
+
+            self.filter_label.setText("Data is Not Currently Filtered")
 
